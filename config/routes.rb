291d0e54d0e519ce16666bf09a81bed53cf1330d
DistractedPrimate <m_medearis@ucsb.edu>
2023-02-07 15:56:46 -0800

    added darkmode

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Point route to a controller
 
-  get "about", to: 'about#index'
+  get "about", to: 'about#index', as: :about
 
   root to: "main#index"
   # Defines the root path route ("/")
