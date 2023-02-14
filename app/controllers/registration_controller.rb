a2db2683e06aa3dac0c7f7f131c973d150a5f6be
DistractedPrimate <m_medearis@ucsb.edu>
2023-02-13 18:04:17 -0800

    pt 18

---
+++ app/controllers/registration_controller.rb
@@ -0,0 +1,23 @@
+class RegistrationController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+
+        @user = User.new(user_params)
+
+        if @user.save
+            session[:user_id] = @user.id
+            msg = "Thank You for Signing Up" + @user.username
+            redirect_to root_path, notice: msg
+        else
+            render :new
+        end            
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
+end
+        
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   # Point route to a controller
 
   get "about", to: 'about#index', as: :about
+  get "signup", to: 'registration#new'
+  post "signup", to: 'registration#create'
+
+  get "signin", to: 'sessions#new'
+  post "signin", to: 'sessions#create'
+  delete "logout", to: 'sessions#destroy'
 
   root to: "main#index"
   # Defines the root path route ("/")
